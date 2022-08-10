cb22277f07db20b5c9048c321891195dce82735d
UdderlyEvelyn <udderlyevelyn@gmail.com>
2022-08-10 00:46:03 -0400

    Added check so that the whitelist only applies outside of safezones.

--- Contents/mods/ValhallaAegisSledgehammerWhitelistAddon/media/lua/client/VSA10_ContainerProtection_Addon.lua
+++ Contents/mods/ValhallaAegisSledgehammerWhitelistAddon/media/lua/client/VSA10_ContainerProtection_Addon.lua
@@ -13,19 +13,21 @@ end
 
 Valhalla.ContainerProtection["allowObjectDestruction"] = 
 	function(self, object, player, mode)
-		local objectName = object:getSprite():getName()				
-		--print("[ValhallaAegis] Checking if \""..objectName.."\" is on the sledgehammer whitelist..")
-		if SandboxVars.ValhallaAegis.SledgehammerWhitelist ~= nil and SandboxVars.ValhallaAegis.SledgehammerWhitelist ~= "" then
-			local found = false
-			for i,item in ipairs(split(SandboxVars.ValhallaAegis.SledgehammerWhitelist, ";")) do
-				--print("[ValhallaAegis] Checking against whitelist item: \""..item.."\"..")
-				if item == objectName then
-					return true
+		if not Valhalla:squareIsInSafehouse(object:getSquare()) then --if we're *not* in a safehouse, then the whitelist applies
+			local objectName = object:getSprite():getName()				
+			--print("[ValhallaAegis] Checking if \""..objectName.."\" is on the sledgehammer whitelist..")
+			if SandboxVars.ValhallaAegis.SledgehammerWhitelist ~= nil and SandboxVars.ValhallaAegis.SledgehammerWhitelist ~= "" then
+				local found = false
+				for i,item in ipairs(split(SandboxVars.ValhallaAegis.SledgehammerWhitelist, ";")) do
+					--print("[ValhallaAegis] Checking against whitelist item: \""..item.."\"..")
+					if item == objectName then
+						return true
+					end
 				end
-			end
-			if not found then
-				print("[ValhallaAegis] \""..objectName.."\" is not on the sledgehammer whitelist..")
-				return false
+				if not found then
+					print("[ValhallaAegis] \""..objectName.."\" is not on the sledgehammer whitelist..")
+					return false
+				end
 			end
 		end
 		return original(self, object, player, mode)
